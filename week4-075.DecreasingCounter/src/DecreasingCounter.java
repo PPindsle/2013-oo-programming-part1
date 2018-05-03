c67b862eb3b2579e7e715dd6a2bfb85071449b81
PPindsle <38837326+PPindsle@users.noreply.github.com>
2018-05-03 15:53:27 +0200

    Add files via upload

---
+++ week4-075.DecreasingCounter/src/DecreasingCounter.java
@@ -0,0 +1,27 @@
+public class DecreasingCounter {
+    private int value;  // instance variable that remembers the value of the counter
+    private int initialValue;
+    
+    public DecreasingCounter(int valueAtStart) {
+        this.value = valueAtStart;
+        this.initialValue = valueAtStart;
+    }
+
+    public void printValue() {
+        System.out.println("value: " + this.value);
+    }
+
+    public void decrease() {
+        if (this.value >= 1) {
+            this.value = this.value - 1;
+        }
+    }
+    
+    public void reset() {
+        this.value = 0;
+    }
+    
+    public void setInitial() {
+        this.value = initialValue;
+    }
+}
