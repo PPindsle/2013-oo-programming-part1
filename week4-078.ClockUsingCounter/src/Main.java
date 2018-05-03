cf91774637070b82243d62d44cacfb98dbb68423
PPindsle <38837326+PPindsle@users.noreply.github.com>
2018-05-03 19:22:40 +0200

    Add files via upload

--- week4-078.ClockUsingCounter/src/Main.java
+++ week4-078.ClockUsingCounter/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) /*throws Exception //FOR ETERNAL CLOCK  */ {
         Scanner reader = new Scanner(System.in);
         BoundedCounter seconds = new BoundedCounter(59);
         BoundedCounter minutes = new BoundedCounter(59);
@@ -31,7 +31,24 @@ public class Main {
             if (minutes.getValue() == 0 && seconds.getValue() == 0) {
                 hours.next();
             }
+        
         }
+        /* ETERNAL CLOCK
+        seconds.setValue(50);
+        minutes.setValue(59);
+        hours.setValue(23);
+
+        while ( true ) {
+            System.out.println( hours + ":" + minutes + ":" + seconds );
+            Thread.sleep(1000);
+            seconds.next();
+            if (seconds.getValue() == 0) {
+                minutes.next();
+            }
+            if (minutes.getValue() == 0 && seconds.getValue() == 0) {
+                hours.next();
+            }
+        } */
 
     }
 }
