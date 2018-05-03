b5817f73b760c34ac79904e22fa8d998ea8f70e8
PPindsle <38837326+PPindsle@users.noreply.github.com>
2018-05-03 10:44:59 +0200

    Add files via upload

---
+++ week3-054.TheEndPart/src/TheEndPart.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TheEndPart {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Type a word:");
+        String word = reader.nextLine();
+        System.out.println("Length of end part:");
+        int length = Integer.parseInt(reader.nextLine());
+        int l = word.length() - length;
+        
+        String result = word.substring(l);
+        System.out.println("Result: " + result);
+    }
+}
